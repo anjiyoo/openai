ed935f156daffdfeebacf18fa5358f0f7bc5b237
Yoojian <soiseaubleu@gmail.com>
2024-03-12 10:32:43 +0900

    commit

--- openai_api_key.py
+++ openai_api_key.py
@@ -1,10 +1,10 @@
 from dotenv import load_dotenv
 import os
 
-# .env 파일에서 환경 변수를 로드합니다.
+# .env 파일에서 환경 변수를 로드
 load_dotenv()
 
-# 환경 변수를 사용하여 API 키를 불러옵니다.
+# 환경 변수를 사용하여 API 키를 불러오기
 openai_api_key = os.getenv("OPENAI_API_KEY")
 
 # API 키를 출력하여 확인 (실제 사용시에는 출력하지 않도록 주의)
